e9314e478503e48789f86e6cac33886d79645b07
Scott Lingner <slingner@gmail.com>
2020-03-14 18:18:13 -0700

    kept french but put different words

--- seeds/seed.tables.sql
+++ seeds/seed.tables.sql
@@ -21,14 +21,14 @@ VALUES
 
 INSERT INTO "word" ("id", "language_id", "original", "translation", "next")
 VALUES
-  (1, 1, 'entraine toi', 'practice', 2),
-  (2, 1, 'bonjour', 'hello', 3),
-  (3, 1, 'maison', 'house', 4),
-  (4, 1, 'développeur', 'developer', 5),
-  (5, 1, 'traduire', 'translate', 6),
-  (6, 1, 'incroyable', 'amazing', 7),
-  (7, 1, 'chien', 'dog', 8),
-  (8, 1, 'chat', 'cat', null);
+  (1, 1, 'ange', 'angel', 2),
+  (2, 1, 'baleine', 'whale', 3),
+  (3, 1, 'bisou', 'kiss', 4),
+  (4, 1, 'brindille', 'twig', 5),
+  (5, 1, 'brûler', 'to burn', 6),
+  (6, 1, 'brume', 'mist', 7),
+  (7, 1, 'câlin', 'hug', 8),
+  (8, 1, 'chaleur', 'heat', null);
 
 UPDATE "language" SET head = 1 WHERE id = 1;
 
