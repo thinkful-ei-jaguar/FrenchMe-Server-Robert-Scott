4723e7fa5253c39735d14706b953d09a87a27364
Alex <robert.perez6@g.austincc.edu>
2020-03-17 16:08:43 -0500

    responce on guess

--- src/language/language-router.js
+++ src/language/language-router.js
@@ -151,7 +151,6 @@ languageRouter
           linkarr.push(arrtemp.value);
           arrtemp = arrtemp.next;
         }
-
         // LanguageService.insertNewLinkedList(req.app.get('db'),linkarr);
         // LanguageService.updateLanguagetotalScore(req.app.get('db'),language);
         return res.json(responce),
