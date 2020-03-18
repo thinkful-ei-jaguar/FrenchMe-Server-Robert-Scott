5796e91ad29a78160024269c73601e1f6b6ab8e8
Scott Lingner <slingner@gmail.com>
2020-03-17 21:15:37 -0700

    syntax fixes

--- src/language/language-router.js
+++ src/language/language-router.js
@@ -13,12 +13,10 @@ languageRouter
         req.app.get('db'),
         req.user.id,
       )
-
       if (!language)
         return res.status(404).json({
           error: `You don't have any languages`,
         })
-
       req.language = language
       next()
     } catch (error) {
@@ -33,7 +31,6 @@ languageRouter
         req.app.get('db'),
         req.language.id,
       )
-
       res.json({
         language: req.language,
         words,
@@ -51,7 +48,7 @@ languageRouter
       req.app.get('db'),
       req.language.id,
     )
-    const firstword=words[0]
+    const firstword = words[0]
     res.json({
       firstword,
     })
@@ -76,12 +73,10 @@ languageRouter
         req.app.get('db'),
         req.user.id,
       )
-      let responce = {
+      let response = {
           nextWord: words[1].original,  
       }
 
-
-  
       //check if right or wrong
      if(guess == link.head.value.translation){
         console.log("they got their answer right")
@@ -90,71 +85,71 @@ languageRouter
         //add 1 to the correct counter
         link.head.value.correct_count++;
         //add 1 to the total score counter
-        console.log("ok");
-        language.total_score+=1;
-        let increment=words[0].correct_count;
+        language.total_score += 1;
+        let increment = words[0].correct_count;
         increment++;
-        responce={...responce,
+
+        response = {...response,
           wordCorrectCount: increment,
           wordIncorrectCount: words[0].incorrect_count,
           totalScore: language.total_score,
           answer: words[0].translation,
-          isCorrect:true,
+          isCorrect: true,
         }
-      }else{
+      } else {
         link.head.value.incorrect_count++;
         link.head.value.memory_value = 1;
         let decrement=words[0].incorrect_count;
         decrement++;
-        responce={...responce,
+        
+        response = {...response,
           wordCorrectCount: words[0].correct_count,
           wordIncorrectCount: decrement,         
           totalScore: language.total_score,
           answer: words[0].translation,
           isCorrect:false,
         }
+
       }
         //push from list
         m = link.head.value.memory_value;
         temp = link.head;
         //while head && mem val is less than 0
         while(temp && m > 0){
-          //first temp original value 1-2-3
-          //2-1-3
-          let toriginal =temp.value.original;
-          let ttranslation=temp.value.translation;
-          let tcorrect_count=temp.value.correct_count;
-          let tincorrect_count=temp.value.incorrect_count;
+          //first temp original 
+          let toriginal = temp.value.original;
+          let ttranslation = temp.value.translation;
+          let tcorrect_count = temp.value.correct_count;
+          let tincorrect_count = temp.value.incorrect_count;
           let tm  = temp.value.memory_value;
-          //change that value to the next 2-2-3
-          //2-3-3
-          temp.value.original=temp.next.value.original;
-          temp.value.translation=temp.next.value.translation;
-          temp.value.correct_count=temp.next.value.correct_count;
-          temp.value.incorrect_count=temp.next.value.incorrect_count;
-          temp.value.memory_value=temp.next.value.memory_value;
-          //replace the value 2-2-3
-          //replace the value 2-3-3
-          temp.next.value.original=toriginal;
-          temp.next.value.translation=ttranslation;
-          temp.next.value.correct_count=tcorrect_count;
-          temp.next.value.incorrect_count=tincorrect_count;
-          temp.next.value.memory_value=tm;
-          temp=temp.next;
-          //2-1-3
-          //2-3-1
+
+          temp.value.original = temp.next.value.original;
+          temp.value.translation = temp.next.value.translation;
+          temp.value.correct_count = temp.next.value.correct_count;
+          temp.value.incorrect_count = temp.next.value.incorrect_count;
+          temp.value.memory_value = temp.next.value.memory_value;
+
+          temp.next.value.original = toriginal;
+          temp.next.value.translation = ttranslation;
+          temp.next.value.correct_count = tcorrect_count;
+          temp.next.value.incorrect_count = tincorrect_count;
+          temp.next.value.memory_value = tm;
+          temp = temp.next;
           m--;
         }
         //if we want an array
-        arrtemp=link.head;
-        linkarr=[]
+        arrtemp = link.head;
+        linkarr = [];
+
         while(arrtemp){
           linkarr.push(arrtemp.value);
           arrtemp = arrtemp.next;
         }
+
         LanguageService.insertNewLinkedList(req.app.get('db'),linkarr);
         LanguageService.updateLanguagetotalScore(req.app.get('db'),language);
-        return res.json(responce),
+
+        return res.json(response),
         next();
     } catch (error) {
       next(error)
